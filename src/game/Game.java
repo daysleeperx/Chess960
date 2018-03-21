f54d603529f08faddd75a0715014b5d550d4ce18
Viktor Pavlov <vvpavlov19@gmail.com>
2018-03-21 13:58:45 +0200

    fix #5

--- src/game/Game.java
+++ src/game/Game.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import static game.Parser.parseInput;
+
 /**
  * Represent Game.
  */
@@ -16,30 +18,15 @@ public class Game {
     public void game() {
         Board b = new Board();
         b.setUpPieces();
-        Map<Character, Integer> sq = new HashMap<>();
-        sq.put('a', 0);
-        sq.put('b', 1);
-        sq.put('c', 2);
-        sq.put('d', 3);
-        sq.put('e', 4);
-        sq.put('f', 5);
-        sq.put('g', 6);
-        sq.put('h', 7);
         while (true) {
             b.printGame();
             Scanner sc = new Scanner(System.in);
             String in = sc.nextLine();
             if (in.equals("quit")) break;
-            char[] inputArray = in.toCharArray();
-            int col = sq.get(inputArray[0]);
-            int row = inputArray[1] - '0' - 1;
-            int row2 = inputArray[3] - '0' - 1;
-            int col2 = sq.get(inputArray[2]);
-            Piece current = b.boardArray[row][col].getPiece();
-            b.boardArray[row2][col2].setPiece(current);
-            b.boardArray[row][col].setPiece(null);
-
-
+            int[] m = parseInput(in);
+            Piece current = b.boardArray[m[1]][m[0]].getPiece();
+            b.boardArray[m[3]][m[2]].setPiece(current);
+            b.boardArray[m[1]][m[0]].setPiece(null);
         }
     }
 
--- src/game/Parser.java
+++ src/game/Parser.java
@@ -1,4 +1,37 @@
 package game;
 
-public class Parser {
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Parses user input into chess notation.
+ */
+
+final class Parser {
+    private static Map<Character, Integer> sq = new HashMap<>();
+
+    /**
+     * Class constructor.
+     */
+    private Parser() {
+
+    }
+
+    public static int[] parseInput(String input) {
+        sq.put('a', 0);
+        sq.put('b', 1);
+        sq.put('c', 2);
+        sq.put('d', 3);
+        sq.put('e', 4);
+        sq.put('f', 5);
+        sq.put('g', 6);
+        sq.put('h', 7);
+        int[] out = new int[4];
+        char[] inputArray = input.toCharArray();
+        out[0] = sq.get(inputArray[0]);
+        out[1] = inputArray[1] - '0' - 1;
+        out[2] = sq.get(inputArray[2]);
+        out[3] = inputArray[3] - '0' - 1;
+        return out;
+    }
 }
